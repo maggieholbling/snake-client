53a873774b16f69cb40fc11554b559e00a5a5428
Maggie <maggieholbl@gmail.com>
2019-08-28 22:15:38 +0000

    making snake move

--- input.js
+++ input.js
@@ -1,16 +1,32 @@
-const setupInput = function() {
+// Stores the active TCP connection object.
+let connection;
+
+const setupInput = function(conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
   stdin.resume();
-  handleUserInput(stdin);
+  handleUserInput(stdin, conn);
   return stdin;
  }
 
-const handleUserInput = (stdin) => {
+const handleUserInput = (stdin, conn) => {
   stdin.on('data', (key) => {
     if (key === '\u0003') {
       process.exit();
+    }
+    if (key === 'w' || key === '\u001b[A') {
+      conn.write('Move: up');
+    }
+    if (key === 'a' || key === '\u001b[D') {
+      conn.write('Move: left');
+    }
+    if (key === 's' || key === '\u001b[B') {
+      conn.write('Move: down');
+    }
+    if (key === 'd' || key === '\u001b[C') {
+      conn.write('Move: right');
     }
   });
 }
--- play.js
+++ play.js
@@ -4,6 +4,5 @@ const { setupInput } = require('./input');
  * Establishes connection with the game server
  */
 console.log('Connecting ...');
-connect();
 
-setupInput();
+setupInput(connect());
