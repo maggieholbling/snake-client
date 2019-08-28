0abc25dca6c5dc5ce8908bd2e0df8040a470564f
Maggie <maggieholbl@gmail.com>
2019-08-28 21:01:36 +0000

    added identifier to snake

--- client.js
+++ client.js
@@ -11,6 +11,14 @@ const connect = function() {
     console.log('Server says: ', data);
   });
 
+  conn.on("connect", () => {
+    console.log("Successfully connected to game server");
+  })
+
+  conn.on('connect', () => {
+    conn.write('Name: MGS');
+  });
+
   return conn;
 }
 
