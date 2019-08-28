a473a746e3cb63a8be436d9d36e927c9bd09ada8
Maggie <maggieholbl@gmail.com>
2019-08-28 21:46:43 +0000

    added exit function

--- play.js
+++ play.js
@@ -1,9 +1,25 @@
 const { connect } = require('./client');
-
 /**
  * Establishes connection with the game server
  */
+ const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  handleUserInput(stdin);
+  return stdin;
+ }
 
+  const handleUserInput = (stdin) => {
+    stdin.on('data', (key) => {
+      if (key === '\u0003') {
+        process.exit();
+      }
+    });
+  }
+
+setupInput();
 
 console.log('Connecting ...');
 connect();
